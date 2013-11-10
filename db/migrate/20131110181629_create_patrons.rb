79aff2b0bb27c9c24d04541b19d8e7088411c7c0
jbfink <john.fink@gmail.com>
2013-11-10 18:31:33 -0500

    Fleshed out patron migration.

--- db/migrate/20131110181629_create_patrons.rb
+++ db/migrate/20131110181629_create_patrons.rb
@@ -1,7 +1,15 @@
 class CreatePatrons < ActiveRecord::Migration
   def up
+	  create_table :patrons do |p|
+		  t.string :firstname
+		  t.string :lastname
+		  t.string :department
+		  t.string :notes
+		  t.timestamps
   end
-
+  Patron.create(firstname: "Bob", lastname: "Smith", department: "Libraries", notes: "Bob Smith is a Librarian")
+  end
   def down
+	  drop_table :patrons
   end
 end
