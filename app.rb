65146cb6c15cfa3a05d3dee4af29d3ccc75af2ae
jbfink <john.fink@gmail.com>
2013-11-11 10:21:46 -0500

    Added about haml, views, modified app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,26 @@ set :database, "sqlite3:///crm.db"
 
 class Patron < ActiveRecord::Base
 end
+
+helpers do
+	# If @title is assigned, add it to the page's title
+
+	def title
+		if @title
+			"#{@title} -- Patron CRM"
+		else
+			"Patron CRM"
+		end
+	end
+end
+
+
+get '/' do
+	@patrons = Patron.order(created_at: :desc)
+	erb :"posts/index"
+end
+
+get '/about' do
+	@title = "About Patron CRM"
+	haml :"pages/about"
+end
